28ee4800281347ce682383156cb1a675ebbcbcaf
andy c <t2ahc@sad-panda.local>
2013-12-16 23:31:23 -0500

    build subprocess version of speaker

---
+++ subprocess.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char **argv)
+{
+  int i;
+  for (i=1; i<argc; ++i) {
+    pid_t pid = fork();
+    if (pid == 0) {
+      execl ("/usr/bin/say", "say", argv [i], (char *) NULL);
+    }
+  }
+
+  int status;
+  for (i=1; i<argc; ++i) {
+    pid_t wpid = wait (&status);
+  }
+  return 0;
+}
